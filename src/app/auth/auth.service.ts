27f2951a280cc940cfd4bca86940f9ab8d5499d5
Duc Anh Trinh <ducanh.trinh@deecoob.com>
2018-12-18 14:04:01 +0100

    added jokes

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -42,6 +42,7 @@ export class AuthService {
         map((res: any) => {
           this.user = res.data.login;
           window.localStorage.setItem('token', this.user.jwtToken.token);
+          window.localStorage.setItem('userId', this.user.id.toString());
           return this.user;
         })
       );
@@ -58,12 +59,18 @@ export class AuthService {
       })
       .subscribe(
         _ => {
+          // Logout throws error when token expired?
           window.localStorage.removeItem('token');
         },
         err => {
           console.error(err);
+          // window.localStorage.removeItem('token');
         }
       );
+  }
+
+  getCurrentUserId() {
+    return this.user.id || window.localStorage.getItem('userId') || null;
   }
 
   isLogged(): boolean {
